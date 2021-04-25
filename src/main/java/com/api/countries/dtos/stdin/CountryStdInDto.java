a4eb2501f291f032ba6f078b55f7dde8f9d70f45
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-24 20:53:21 -0500

    creation and implementation dtos entities

---
+++ src/main/java/com/api/countries/dtos/stdin/CountryStdInDto.java
@@ -0,0 +1,21 @@
+package com.api.countries.dtos.stdin;
+
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class CountryStdInDto {
+
+    @Size(max = 45, min=3, message="The name must be between 3 and 45 characters.")
+    @Pattern(regexp = ".*", message="The name can only have characters of the alphabet.")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
