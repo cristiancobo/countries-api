404056d72e61068145ba82986ec3dbf7a5b43dda
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-24 20:33:57 -0500

    project structure

---
+++ src/main/java/com/api/countries/CountriesApplication.java
@@ -0,0 +1,13 @@
+package com.api.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesApplication.class, args);
+	}
+
+}
