bf07b97ca0fa7d2da2dfea35afe4eaa34901f490
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-26 10:44:46 -0500

    id attribute added to country out dto

--- src/main/java/com/api/countries/dtos/stdout/CountryStdOutDto.java
+++ src/main/java/com/api/countries/dtos/stdout/CountryStdOutDto.java
@@ -1,18 +1,32 @@
 package com.api.countries.dtos.stdout;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
+/**
+ * Class representing a country dto
+ * @version 1.0
+ */
 public class CountryStdOutDto {
 
-    @Size(max = 45, min=3, message="The name must be between 3 and 45 characters.")
-    @Pattern(regexp = ".*", message="The name can only have characters of the alphabet.")
+    //Attributes
+
+    private Long id;
+
     private String name;
 
+    //Access methods
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
